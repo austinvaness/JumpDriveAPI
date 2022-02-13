580c43b05de4b999a02f60456939218fc3909b17
austinvaness <austinvaness@gmail.com>
2022-02-12 21:06:24 -0600

    Initial commit

---
+++ JumpDriveAPI/MyJumpDriveLogic.cs
@@ -0,0 +1,206 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Gui;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using System;
+using System.Collections.Generic;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+
+namespace avaness.JumpDriveAPI
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_JumpDrive), false)]
+    public class MyJumpDriveLogic : MyGameLogicComponent
+    {
+        private IMyJumpDrive block;
+        private Vector3D? target;
+        private static bool controls;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            block = (IMyJumpDrive)Entity;
+            NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            if (block.CubeGrid?.Physics == null) // ignore projected and other non-physical grids
+                return;
+
+            if(!controls)
+            {
+                CreateControls();
+                controls = true;
+            }
+        }
+
+        private static void CreateControls()
+        {
+            // Properties
+
+            IMyTerminalControlProperty<Vector3D?> propScriptTarget = MyAPIGateway.TerminalControls.CreateProperty<Vector3D?, IMyJumpDrive>("ScriptJumpTarget");
+            propScriptTarget.Getter = GetJumpTarget;
+            propScriptTarget.Setter = SetJumpTarget;
+            MyAPIGateway.TerminalControls.AddControl<IMyJumpDrive>(propScriptTarget);
+
+            IMyTerminalControlProperty<Vector3D?> propRealTarget = MyAPIGateway.TerminalControls.CreateProperty<Vector3D?, IMyJumpDrive>("JumpTarget");
+            propRealTarget.Getter = GetRealJumpTarget;
+            propRealTarget.Setter = (b, v) => { };
+            MyAPIGateway.TerminalControls.AddControl<IMyJumpDrive>(propRealTarget);
+
+
+            // Actions
+
+            IMyTerminalAction actionJump = MyAPIGateway.TerminalControls.CreateAction<IMyJumpDrive>("ScriptJump");
+            actionJump.ValidForGroups = false;
+            actionJump.Action = PerformJump;
+            actionJump.Name = new System.Text.StringBuilder("Script Jump");
+            actionJump.InvalidToolbarTypes = new List<MyToolbarType>() { MyToolbarType.Character, MyToolbarType.SmallCockpit, MyToolbarType.LargeCockpit, MyToolbarType.Ship, MyToolbarType.Seat, MyToolbarType.Spectator, MyToolbarType.BuildCockpit }; // All except button panel
+            MyAPIGateway.TerminalControls.AddAction<IMyJumpDrive>(actionJump);
+
+            IMyTerminalAction actionAbort = MyAPIGateway.TerminalControls.CreateAction<IMyJumpDrive>("AbortJump");
+            actionAbort.ValidForGroups = false;
+            actionAbort.Action = AbortJump;
+            actionAbort.Name = new System.Text.StringBuilder("Abort Jump");
+            actionAbort.InvalidToolbarTypes = new List<MyToolbarType>() { MyToolbarType.Character, MyToolbarType.SmallCockpit, MyToolbarType.LargeCockpit, MyToolbarType.Ship, MyToolbarType.Seat, MyToolbarType.Spectator, MyToolbarType.BuildCockpit }; // All except button panel
+            MyAPIGateway.TerminalControls.AddAction<IMyJumpDrive>(actionAbort);
+
+        }
+
+        private static void AbortJump(IMyTerminalBlock block)
+        {
+            if (MyAPIGateway.Session?.IsServer != true)
+                return;
+            
+            IMyGridJumpDriveSystem jumpSystem = block.CubeGrid?.JumpSystem;
+            if (jumpSystem == null || !jumpSystem.IsJumping)
+                return;
+
+            jumpSystem.AbortJump();
+        }
+
+        private static void PerformJump(IMyTerminalBlock block)
+        {
+            if (MyAPIGateway.Session?.IsServer != true)
+                return;
+
+            MyJumpDriveLogic logic = block.GameLogic.GetAs<MyJumpDriveLogic>();
+            if (logic == null || !logic.target.HasValue)
+                return;
+
+            IMyGridJumpDriveSystem jumpSystem = block.CubeGrid?.JumpSystem;
+            if (jumpSystem == null || jumpSystem.IsJumping)
+                return;
+
+            IMyJumpDrive jumpDrive = (IMyJumpDrive)block;
+            if (jumpDrive.Status != Sandbox.ModAPI.Ingame.MyJumpDriveStatus.Ready)
+                return;
+
+            Vector3D target = logic.target.Value;
+            if(logic.IsJumpValid(ref target))
+                jumpSystem.Jump(target, block.OwnerId);
+        }
+
+        private static void SetJumpTarget(IMyTerminalBlock block, Vector3D? pos)
+        {
+            MyJumpDriveLogic logic = block.GameLogic.GetAs<MyJumpDriveLogic>();
+            if (logic == null)
+                return;
+
+            logic.target = pos;
+        }
+
+        private bool IsJumpValid(ref Vector3D jumpTarget)
+        {
+            IMyCubeGrid grid = block.CubeGrid;
+            IMyGridJumpDriveSystem jumpSystem = grid.JumpSystem;
+            long userId = block.OwnerId;
+            Vector3D gridPos = grid.WorldMatrix.Translation;
+
+            // Check if grid is immovable
+            if (!jumpSystem.IsJumpValid(userId))
+                return false;
+
+            // Check if the grid is leaving or entering gravity
+            if (IsWithinGravity(jumpTarget) || IsWithinGravity(gridPos))
+                return false;
+
+            // Check if the jump is within the world size
+            if(!MyEntities.IsInsideWorld(jumpTarget))
+                return false;
+
+            // Check if a planet is in the way
+            IMyEntity intersection = GetIntersectionWithLine(gridPos, jumpTarget);
+            if(intersection != null)
+            {
+                if (intersection is MyPlanet)
+                    return false;
+
+                Vector3D point = intersection.WorldMatrix.Translation;
+                Vector3D closestPointOnLine = MyUtils.GetClosestPointOnLine(ref gridPos, ref jumpTarget, ref point);
+                float halfExtents = intersection.PositionComp.LocalAABB.HalfExtents.Length();
+                jumpTarget = closestPointOnLine - Vector3D.Normalize(jumpTarget - gridPos) * halfExtents;
+            }
+
+            // Check if there is an available place to jump to
+            Vector3D? newTarget = jumpSystem.FindSuitableJumpLocation(jumpTarget);
+            if (!newTarget.HasValue)
+                return false;
+            jumpTarget = newTarget.Value;
+
+            // Check if grid is too close or too far
+            double distance = (jumpTarget - gridPos).Length();
+            if (distance < jumpSystem.GetMinJumpDistance(userId) || distance > jumpSystem.GetMaxJumpDistance(userId))
+                return false;
+
+            return true;
+        }
+
+        private IMyEntity GetIntersectionWithLine(Vector3D start, Vector3D end)
+        {
+            LineD line = new LineD(start, end);
+            VRage.Game.Models.MyIntersectionResultLineTriangleEx? intersectionWithLine = MyEntities.GetIntersectionWithLine(ref line, (MyEntity)block.CubeGrid, null, ignoreChildren: true, ignoreFloatingObjects: true, ignoreHandWeapons: true, ignoreObjectsWithoutPhysics: false, ignoreSubgridsOfIgnoredEntities: true);
+
+            if (!intersectionWithLine.HasValue)
+                return null;
+            return intersectionWithLine.Value.Entity;
+        }
+
+        private bool IsWithinGravity(Vector3D pos)
+        {
+            MyPlanet planet = MyGamePruningStructure.GetClosestPlanet(pos);
+            return planet != null && IsWithinGravity(pos, planet);
+        }
+
+        private bool IsWithinGravity(Vector3D pos, MyPlanet planet)
+        {
+            MyGravityProviderComponent gravity = planet.Components.Get<MyGravityProviderComponent>();
+            return gravity != null && gravity.IsPositionInRange(pos);
+        }
+
+        private static Vector3D? GetJumpTarget(IMyTerminalBlock block)
+        {
+            MyJumpDriveLogic logic = block.GameLogic.GetAs<MyJumpDriveLogic>();
+            if (logic == null)
+                return null;
+
+            return logic.target;
+        }
+
+        private static Vector3D? GetRealJumpTarget(IMyTerminalBlock block)
+        {
+            IMyGridJumpDriveSystem jumpSystem = block.CubeGrid?.JumpSystem;
+            if (jumpSystem == null)
+                return null;
+
+            return jumpSystem.GetJumpDriveTarget();
+        }
+    }
+}
---
+++ TestScript/Program.cs
@@ -0,0 +1,103 @@
+﻿using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        // ==============================================================
+        //  Instructions
+        // ==============================================================
+        //
+        // To use, simply run the script with a gps waypoint as an argument.
+        // The script will then attempt to jump to the destination.
+        //
+        // You may also put "abort" (without quotes) into the argument
+        // box to abort any active jump.
+        // 
+        // ==============================================================
+        //  Settings
+        // ==============================================================
+        //
+        // Replace the value in quotes with the name of your jump drive block.
+        private const string jumpDriveName = "Jump Drive";
+        //
+        // ==============================================================
+
+
+        private IMyJumpDrive jumpDrive;
+        public Program()
+        {
+            jumpDrive = (IMyJumpDrive)GridTerminalSystem.GetBlockWithName(jumpDriveName);
+            if(jumpDrive == null)
+            {
+                List<IMyJumpDrive> drives = new List<IMyJumpDrive>();
+                GridTerminalSystem.GetBlocksOfType<IMyJumpDrive>(drives, b => drives.Count == 0);
+                jumpDrive = drives.FirstOrDefault();
+            }
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if (jumpDrive == null)
+            {
+                Echo("No jump drive!");
+                return;
+            }
+
+            if (argument.Length == 5 && argument.ToLowerInvariant() == "abort")
+                AbortJump();
+            else
+                Jump(argument);
+
+        }
+
+        private void Jump(string argument)
+        {
+            if (jumpDrive.Status != MyJumpDriveStatus.Ready)
+            {
+                Echo("Jump drive is not ready!");
+                return;
+            }
+
+            Vector3D pos;
+            if (TryParseGPS(argument.Trim(), out pos))
+            {
+                Echo("Jumping to " + argument);
+                jumpDrive.SetValue<Vector3D?>("ScriptJumpTarget", pos);
+                jumpDrive.ApplyAction("ScriptJump");
+            }
+            else
+            {
+                Echo("Unable to parse " + argument + " into a gps coordinate.");
+            }
+        }
+
+        private void AbortJump()
+        {
+            Echo("Aborted jump.");
+            jumpDrive.ApplyAction("AbortJump");
+        }
+
+        private bool TryParseGPS(string argument, out Vector3D pos)
+        {
+            pos = new Vector3D();
+
+            //GPS:name:x:y:z:color:
+            string[] args = argument.Split(':');
+            if (args.Length < 5 || args[0] != "GPS")
+                return false;
+
+            double x, y, z;
+            if (!double.TryParse(args[2], out x) || !double.TryParse(args[3], out y) || !double.TryParse(args[4], out z))
+                return false;
+
+            pos = new Vector3D(x, y, z);
+            return true;
+        }
+    }
+}
